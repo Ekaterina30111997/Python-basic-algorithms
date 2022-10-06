4d15f2d494c65e0deb4b42ba884042367d35c116
Ekaterina30111997 <106620904+Ekaterina30111997@users.noreply.github.com>
2022-10-06 15:57:04 +0300

    Add files via upload

---
+++ Example №3.py
@@ -0,0 +1,8 @@
+arr_a = [3, 8, 15, 16, 18, 19, 20, 24, 30, 33, 36, 40]
+for i in range (len(arr_a)):
+    if arr_a[i] % 3 == 0:
+        print(arr_a[i])
+    else:
+        print("Wrong number")
+
+
